8ad7927481ec6aa6eb9723cb6320c148c2f12aeb
Zakkiyyat <zakkiyyatisa@gmail.com>
2026-07-29 15:21:36 +0100

    Fix: add missing font optimisation — replace any google fonts link tags with next/font (#622)
    * Fix: add missing font optimisation — replace any google fonts link tags with next/font
    
    * Fix: add missing font optimisation — replace any google fonts link tags with next/font
    
    ---------
    
    Co-authored-by: aaseenib <aaseenib@users.noreply.github.com>
    Co-authored-by: S-Mubarak <S-Mubarak@users.noreply.github.com>

---
+++ src/utils/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Roboto, Montserrat } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
+export const roboto = Roboto({ weight: ['400', '700'], subsets: ['latin'], variable: '--font-roboto' });
+export const montserrat = Montserrat({ subsets: ['latin'], variable: '--font-montserrat' });
